44f886688750acac539ad223f6db55dc94be56e8
iya <mrzhwayne@163.com>
2022-12-03 21:56:05 +0800

    修复 MyActionLayout 动画异常

--- Example/EasyAlert/MyActionLayout.swift
+++ Example/EasyAlert/MyActionLayout.swift
@@ -6,19 +6,26 @@
 //  Copyright © 2022 CocoaPods. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import EasyAlert
 
 struct MyActionLayout : ActionLayoutable {
+    
+    private let stackView: UIStackView
+    
+    init() {
+        stackView = UIStackView()
+        stackView.distribution = .fillEqually
+        stackView.spacing = 16
+    }
 
     func layout(actionViews: [UIView], container: UIView) {
-
-        let stackView = UIStackView(arrangedSubviews: actionViews)
+        stackView.arrangedSubviews.forEach { $0.removeFromSuperview() }
+        actionViews.forEach { stackView.addArrangedSubview($0) }
         stackView.axis = actionViews.count <= 3 ? .horizontal : .vertical
-        stackView.distribution = .fillProportionally
-        stackView.spacing = 16
-        container.addSubview(stackView)
         
+        NSLayoutConstraint.deactivate(stackView.constraints)
+        container.addSubview(stackView)
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.topAnchor.constraint(equalTo: container.topAnchor, constant: 0).isActive = true
         stackView.leftAnchor.constraint(equalTo: container.leftAnchor, constant: 16).isActive = true
